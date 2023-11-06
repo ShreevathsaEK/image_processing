276bb09102b8e3c59f5aade8121b670539d90ed5
ShreevathsaEK <126555689+ShreevathsaEK@users.noreply.github.com>
2023-11-06 09:26:55 +0530

    Create test.m

---
+++ test.m
@@ -0,0 +1,17 @@
+% Load and preprocess a new image (adjust as needed)
+newImage = imread("DEER3.jpg");
+newImage = imresize(newImage, [32, 32]);
+newImage = double(newImage) / 255;
+
+
+% prediction
+predictedLabel = classify(net, newImage);
+
+% Create a custom figure to display the image
+fig = figure('Name', 'Prediction Result', 'NumberTitle', 'off');
+imshow(newImage);
+title(sprintf('Predicted Label: %s', char(predictedLabel)));
+
+
+% Wait for the user to close the figure
+uiwait(fig);
